65fff7e7f544598a8e2fffbd0c95a6e1c9f65300
Ruben <your_email@example.com>
2026-05-05 19:37:12 -0600

    feat: all files refactored

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from src.core.visualizer import plot_return_density_with_var
-from src.core.quant_engine import run_quant_engine
-from src.core.extractor import run_etl_pipeline
-from database.db_config import get_db_engine
 from config.js_config import get_js_config
-    
-def execute_dashboard() -> None:
-    """Execute the visualization dashboard.
+from database.db_config import get_db_engine
+from src.core.extractor import run_etl_pipeline
+from src.core.quant_engine import run_quant_engine
+from src.core.visualizer import plot_return_density_with_var
+from src.utils.logger import setup_logging
 
-    This function orchestrates the quantitative analysis process. It calls the quant
+logger = setup_logging(__name__)
+
+
+def execute_dashboard() -> None:
+    """Executes the visualization dashboard.
+
+    Orchestrates the quantitative analysis process. It calls the quant
     engine to retrieve the values from its dictionary and passes them to the visualizer 
     to render the histogram.
 
@@ -23,19 +27,27 @@ def execute_dashboard() -> None:
     """
     data = get_js_config()
     engine = get_db_engine()
-    run_etl_pipeline(data,engine)
-    quant_engine_dictionary = run_quant_engine(data,engine)
-    portfolio_percentages_changes = quant_engine_dictionary["portfolio_percentages_changes"]
+
+    run_etl_pipeline(data, engine)
+    quant_engine_dictionary = run_quant_engine(data, engine)
+
+    portfolio_percentage_changes = quant_engine_dictionary["portfolio_percentage_changes"]
     portfolio_mean = quant_engine_dictionary["portfolio_mean"]
     portfolio_vol = quant_engine_dictionary["portfolio_vol"]
     var_value = quant_engine_dictionary["var_value"]
     confidence_level = quant_engine_dictionary["confidence_level"]
-    plot_return_density_with_var(portfolio_percentages_changes, portfolio_mean, portfolio_vol, var_value, confidence_level)
+
+    plot_return_density_with_var(
+        portfolio_percentage_changes, 
+        portfolio_mean, portfolio_vol, 
+        var_value, confidence_level
+    )
+
 
 def main() -> None:
     """Entry point for the dashboard application.
 
-    This function triggers the execution of the main dashboard logic.
+    Triggers the execution of the main dashboard logic.
 
     Args:
         None: This function does not have arguments.
@@ -47,8 +59,9 @@ def main() -> None:
         None: This function does not have raises.
 
     """
+    logger.info("Initializing Value at Risk dashboard application...")
     execute_dashboard()
-    print("Dashboard has been executed successfully.")
+    logger.info("Value at Risk dashboard executed successfully.")
 
 if __name__ == "__main__":
     main()
